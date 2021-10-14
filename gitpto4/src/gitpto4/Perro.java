35a0c1fe6ab8d332b4730b5c7e62f565c9f95878
Danito90 <daniel_14_559@hotmail.com>
2021-10-14 19:26:49 -0300

    1- Creo clase Caballo,Gato,Perro

---
+++ gitpto4/src/gitpto4/Perro.java
@@ -0,0 +1,18 @@
+package gitpto4;
+
+/**
+ *
+ * @author daniel
+ */
+public class Perro {
+    private int id;
+    private String nombre;
+    private String color;
+    private double peso;
+    private String altura;
+    private String dueño;
+    private String raza;
+    private int edad;
+    
+    
+}
